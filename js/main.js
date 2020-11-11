78ba208a598a286a83f3eb6f0773a4b9000071d1
battysauiro <battyguide8428@gmail.com>
2020-11-11 15:32:45 -0600

    se acabo de agregar el slides

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, {
+        duration: 100,
+        dist: -80,
+        shift: 5,
+        padding:5,
+        numVisible: 4,
+        indicators:true
+    });
+  });
